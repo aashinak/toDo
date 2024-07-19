5fdf6b62c93c6cd6953195cd2e9a27582c8f6ab6
Aashin A K <aashin9605@gmail.com>
2024-07-19 12:17:30 +0530

    fix 1.3

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,27 +1,29 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import userRoute from "./routes/user.routes.js";
 import { responseHandler } from "./middlewares/responseHandler.js";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import { serverErrorHandler } from "./middlewares/serverErrorHandler.js";
 import todoRoute from "./routes/todo.routes.js";
 
-
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: process.env.CORS_ORIGIN
-}))
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.CORS_ORIGIN,
+        sameSite: "Strict",
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // handler for api responses
-app.use(responseHandler)
-app.use(cookieParser())
+app.use(responseHandler);
+app.use(cookieParser());
 
-app.use("/users", userRoute)
-app.use("/todo", todoRoute)
+app.use("/users", userRoute);
+app.use("/todo", todoRoute);
 
 // handler for server side errors
-app.use(serverErrorHandler)
+app.use(serverErrorHandler);
 export { app };
